802bd02c0e019176876a4ddb46b40c89d36006f7
Ausha123 <aushasandeepa569@gmail.com>
2023-07-12 14:58:42 +0530

    slider done

---
+++ src/utils/comman.js
@@ -0,0 +1,18 @@
+export const sliderSettings = {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    750: {
+      slidesPerView: 3,
+    },
+    1100: {
+      sliderPerView: 4,
+    },
+  },
+};
